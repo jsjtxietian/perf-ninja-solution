c9c4e76c4426a6228c5e9e3f8a07ac78aeb29295
jsjtxietian <jsjtxietian@outlook.com>
2023-06-05 16:35:50 +0800

    warm up

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -1,10 +1,16 @@
 
 #include "solution.h"
 
-int solution(int *arr, int N) {
-  int res = 0;
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
-  }
-  return res;
+int solution(int *arr, int N)
+{
+#ifdef SOLUTION
+	return (N*(N+1))/2;
+#else
+	int res = 0;
+	for (int i = 0; i < N; i++)
+	{
+		res += arr[i];
+	}
+	return res;
+#endif
 }
