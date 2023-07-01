55a5f114307896712d6155ce0d88116f2590385d
jsjtxietian <jsjtxietian@outlook.com>
2023-07-01 14:02:00 +0800

    virtual call solution

--- PersonalNote.md
+++ PersonalNote.md
@@ -2,4 +2,13 @@ quick start
 * install python packages listed in checl_speedup.py
 * install gbench use scrip under tools folder
 * install clang and ninja
+* code ```#ifdef SOLUTION```
 * run ```python .\tools\check_speedup.py -lab_path .\labs\misc\warmup\ -num_runs 3```
+```
+cmake -E make_directory build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release --parallel 8
+cmake --build . --target validateLab
+cmake --build . --target benchmarkLab
+```
--- labs/bad_speculation/virtual_call_mispredict/solution.cpp
+++ labs/bad_speculation/virtual_call_mispredict/solution.cpp
@@ -2,6 +2,45 @@
 
 #include <random>
 
+#ifdef SOLUTION
+
+void generateObjects(InstanceArray& array) {
+    std::default_random_engine generator(0);
+    std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
+
+    int count0 = 0,count1 = 0,count2 = 0;
+
+    for (std::size_t i = 0; i < N; i++) {
+        int value = distribution(generator);
+        if (value == 0) {
+            count0++;
+        } else if (value == 1) {
+            count1++;
+        } else {
+            count2++;
+        }
+    }
+
+    while (count0 != 0)
+    {
+        array.push_back(std::make_unique<ClassA>());
+        count0--;
+    }
+    while (count1 != 0)
+    {
+        array.push_back(std::make_unique<ClassB>());
+        count1--;
+    }
+     while (count2 != 0)
+    {
+        array.push_back(std::make_unique<ClassC>());
+        count2--;
+    }
+    
+}
+
+#else
+
 void generateObjects(InstanceArray& array) {
     std::default_random_engine generator(0);
     std::uniform_int_distribution<std::uint32_t> distribution(0, 2);
@@ -17,6 +56,7 @@ void generateObjects(InstanceArray& array) {
         }
     }
 }
+#endif
 
 // Invoke the `handle` method on all instances in `output`
 void invoke(InstanceArray& array, std::size_t& data) {
