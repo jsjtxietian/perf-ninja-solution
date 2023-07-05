4b13bdce5c0bb60f756c46089a2bc1c49a25e872
jsjtxietian <jsjtxietian@outlook.com>
2023-07-06 00:17:39 +0800

    conditional store

--- labs/bad_speculation/conditional_store_1/solution.cpp
+++ labs/bad_speculation/conditional_store_1/solution.cpp
@@ -6,9 +6,15 @@ std::size_t select(std::array<S, N> &output, const std::array<S, N> &input,
                    const std::uint32_t lower, const std::uint32_t upper) {
   std::size_t count = 0;
   for (const auto item : input) {
+#ifdef SOLUTION
+    bool in_range = (lower <= item.first) && (item.first <= upper);
+    output[count] = item;
+    count += in_range;  // Increment count only if in_range is true.
+#else
     if ((lower <= item.first) && (item.first <= upper)) {
       output[count++] = item;
     }
+#endif
   }
   return count;
 }
