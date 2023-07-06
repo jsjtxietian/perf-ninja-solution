89da4f2b915e8d5086642022db33cb7a139de64e
jsjtxietian <jsjtxietian@outlook.com>
2023-07-06 22:54:51 +0800

    mapped file solution

--- labs/misc/io_opt1/solution.cpp
+++ labs/misc/io_opt1/solution.cpp
@@ -3,7 +3,20 @@
 #include <fstream>
 #include <stdexcept>
 
+#include "MappedFile.hpp"
+
 uint32_t solution(const char *file_name) {
+#ifdef SOLUTION
+
+  // Initial value has all bits set to 1
+  uint32_t crc = 0xff'ff'ff'ff;
+
+  MappedFile mappedFile = MappedFile{file_name};
+  std::string_view content = mappedFile.getContents();
+  for (const auto& c : content) {
+    update_crc32(crc, static_cast<uint8_t>(c));
+  }
+#else
   std::fstream file_stream{file_name};
   if (not file_stream.is_open())
     throw std::runtime_error{"The file could not be opened"};
@@ -19,7 +32,7 @@ uint32_t solution(const char *file_name) {
       break;
     update_crc32(crc, static_cast<uint8_t>(c));
   }
-
+#endif
   // Invert the bits
   crc ^= 0xff'ff'ff'ff;
 
