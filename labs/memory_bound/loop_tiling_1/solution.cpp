1b019714e7de6b13911d71ef273f5c3e85cf11e4
jsjtxietian <jsjtxietian@outlook.com>
2023-07-14 17:28:11 +0800

    update tile solution

--- labs/memory_bound/loop_tiling_1/solution.cpp
+++ labs/memory_bound/loop_tiling_1/solution.cpp
@@ -6,27 +6,16 @@
 bool solution(MatrixOfDoubles &in, MatrixOfDoubles &out) {
   int size = in.size();
 #ifdef SOLUTION
-  constexpr int cachelineSize = std::hardware_constructive_interference_size / sizeof(double);
-  int stride = size / cachelineSize;
-  for (int i = 0; i < stride; i++) {
-    for (int j = 0; j < stride; j++) {
-      for(int m = 0 ; m < cachelineSize ; m++)
-        for(int n = 0 ; n < cachelineSize ; n++)
-          out[i * cachelineSize + m][j* cachelineSize + n] = in[j* cachelineSize + n][i * cachelineSize + m];
+  static constexpr int TILE_SIZE = 16;
+  for (int ii = 0; ii < size; ii+= TILE_SIZE) {
+    for (int jj = 0; jj < size; jj+= TILE_SIZE) {
+      for (int i = ii; i < std::min(ii + TILE_SIZE, size); i++) {
+        for (int j = jj; j < std::min(jj + TILE_SIZE, size); j++) {
+          out[i][j] = in[j][i];
+        }
+      }
     }
   }
-
-  for (int i = cachelineSize * stride ; i < size ;  i++)
-    for (int j = 0; j < size; j++)
-      out[i][j] = in[j][i];
-
-  for (int j = cachelineSize * stride ; j < size ;  j++) 
-    for (int i = 0; i < size; i++) 
-      out[i][j] = in[j][i];
-
-  for (int i = cachelineSize * stride ; i < size ;  i++)
-    for (int j = cachelineSize * stride ; j < size ;  j++) 
-      out[i][j] = in[j][i];
 #else
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
